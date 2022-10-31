61e81286cb585d1e81f167dc85dc7a1e10dbabaa
Makkras2002 <max2002shpak@gmail.com>
2022-10-31 19:03:21 +0300

    request to support ref Upd.

--- src/main/java/com/makkras/shop/entity/RequestToSupport.java
+++ src/main/java/com/makkras/shop/entity/RequestToSupport.java
@@ -23,22 +23,27 @@ public class RequestToSupport extends CustomEntity {
     @JoinColumn(name = "responder_employee_id")
     private User responder;
 
+    @Column(name = "is_completed")
+    private boolean isCompleted;
+
     public RequestToSupport() {
     }
 
-    public RequestToSupport(String requestMessage, String responseMessage, User requester, User responder) {
+    public RequestToSupport(String requestMessage, String responseMessage, User requester, User responder, boolean isCompleted) {
         this.requestMessage = requestMessage;
         this.responseMessage = responseMessage;
         this.requester = requester;
         this.responder = responder;
+        this.isCompleted = isCompleted;
     }
 
-    public RequestToSupport(Long requestId, String requestMessage, String responseMessage, User requester, User responder) {
+    public RequestToSupport(Long requestId, String requestMessage, String responseMessage, User requester, User responder, boolean isCompleted) {
         this.requestId = requestId;
         this.requestMessage = requestMessage;
         this.responseMessage = responseMessage;
         this.requester = requester;
         this.responder = responder;
+        this.isCompleted = isCompleted;
     }
 
     public Long getRequestId() {
@@ -81,6 +86,14 @@ public class RequestToSupport extends CustomEntity {
         this.responder = responder;
     }
 
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -88,6 +101,7 @@ public class RequestToSupport extends CustomEntity {
 
         RequestToSupport that = (RequestToSupport) o;
 
+        if (isCompleted != that.isCompleted) return false;
         if (requestId != null ? !requestId.equals(that.requestId) : that.requestId != null) return false;
         if (requestMessage != null ? !requestMessage.equals(that.requestMessage) : that.requestMessage != null)
             return false;
@@ -104,6 +118,7 @@ public class RequestToSupport extends CustomEntity {
         result = 31 * result + (responseMessage != null ? responseMessage.hashCode() : 0);
         result = 31 * result + (requester != null ? requester.hashCode() : 0);
         result = 31 * result + (responder != null ? responder.hashCode() : 0);
+        result = 31 * result + (isCompleted ? 1 : 0);
         return result;
     }
 
@@ -115,6 +130,7 @@ public class RequestToSupport extends CustomEntity {
         sb.append(", responseMessage='").append(responseMessage).append('\'');
         sb.append(", requester=").append(requester);
         sb.append(", responder=").append(responder);
+        sb.append(", isCompleted=").append(isCompleted);
         sb.append('}');
         return sb.toString();
     }
