e64bfa61cf5e53655201b0c7c03dea7ad3861f79
Makkras2002 <max2002shpak@gmail.com>
2022-11-28 19:26:33 +0300

    war fix Upd.

--- src/main/java/com/makkras/shop/ShopApplication.java
+++ src/main/java/com/makkras/shop/ShopApplication.java
@@ -2,16 +2,17 @@ package com.makkras.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories()
 @EnableScheduling
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
-
 }
